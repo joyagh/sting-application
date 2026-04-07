f5e040dfc45c5ba57a656a5e2f1d884a7e08c553
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-04-07 14:33:09 -0400

    completed steps 1 and 2

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -2,6 +2,23 @@ package com.pluralsight;
 
 public class VariableApp {
     public static void main(String[] args) {
+        //Step 1
+        String favColor ;
+        int classYear;
+        char middleInitial;
+        boolean hasPets;
+        String niceMessage;
+
+//Step 2
+        int weekDays = 7;
+        float coffeePrice = 4.99f;
+        char favLetter = 'J';
+        boolean isRaining = False;
+
+        System.out.println("There are " + weekDays + " in a week!");
+        System.out.println("The price of coffee is " + coffeePrice + ".");
+        System.out.println("My favorite letter is " + favLetter + " !");
+        System.out.println("Is it raining? " + isRaining +".");
 
     }
 }
